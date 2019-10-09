5e76cebde15cb8d0e9844d9f7c3decd0b6ae6e85
VKunze <39528476+VKunze@users.noreply.github.com>
2019-10-09 13:59:54 -0300

    Algunos cambios + inicio BDD

---
+++ script/dbconn.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1997",
+    database : "ticdb"
+});
+
+con.connect(function(err){
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "SELECT * FROM Barrio;";
+    con.query(sql, function(err, result){
+        if (err) throw err;
+        console.log("Result: " + result);
+    });
+});
