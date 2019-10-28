97870597f05ab841186e679db08634c8e0b5abad
Stephanie <smendoncasp@gmail.com>
2019-10-28 16:18:05 -0300

    lnsdvjv

--- script/busquedas.js
+++ script/busquedas.js
@@ -1,9 +1,16 @@
-function ocultar() {
-    document.getElementById('obj1').style.display = 'none';
-}
-
 document.addEventListener("DOMContentLoaded", function(event) {
     document.querySelector("#uruguay").addEventListener("click", function() {
         fetch('uruguay', { method: 'POST' });
+    })
+});
+
+function ocultar() {
+    document.getElementById('obj1').style.display = 'none';
+    document.getElementById('obj1').style.backgroundColor = black;
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.querySelector("#brasil").addEventListener("click", function() {
+        ocultar();
     })
 });
