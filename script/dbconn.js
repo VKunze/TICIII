8b9825970c4aaaa3ab2216170cdfd919c2a2b289
VKunze <39528476+VKunze@users.noreply.github.com>
2019-10-31 17:59:44 -0300

    Cambios vale

--- script/dbconn.js
+++ script/dbconn.js
@@ -1,24 +1,18 @@
 var mysql = require('mysql');
-//var script = require('./globalF.js');
 
-var con = mysql.createConnection({
+var db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "1997",
     database : "ticdb"
 });
 
-//var connectdb = function(){
-//    console.log("att");
-    con.connect(function(err){
-        if (err) throw err;
-        console.log("Connected!");
-        //script.db = con;
-    });
-//};
+/* con.connect(function(err){
+    if (err) throw err;
+    console.log("Connected!");
+}); */
 
-//global.database = con;
 module.exports = {
-    con : con
+    db : db
 };
 
