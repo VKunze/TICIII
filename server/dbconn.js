84af3ab8fd0a4020f3f729cb6bfa19543761db26
VictoriaTejera <victoriatejera15@gmail.com>
2019-11-15 04:05:34 -0300

    Contraseña

--- server/dbconn.js
+++ server/dbconn.js
@@ -3,8 +3,8 @@ var mysql = require('mysql');
   var db = mysql.createConnection({
     host: "localhost",
    user: "root",
-   password: "1997",
-   database : "ticdb"
+   password: "victoria15",
+   database : "usuarios"
 }); 
 
 /* var db = mysql.createConnection({
