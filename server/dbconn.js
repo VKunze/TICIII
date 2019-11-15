4442a46ff166ced9044ad56050684641c8dd0e2f
Stephanie <smendoncasp@gmail.com>
2019-11-15 01:57:40 -0300

    Revert "Merge branch 'master' of https://github.com/VKunze/TICIII"
    This reverts commit 3f5b8a19c14c2e9b1975e775f376b2c3fa92f99c, reversing
    changes made to 6a12fb54946ae506f8bc8f223b638e7bd028e6ce.

--- server/dbconn.js
+++ server/dbconn.js
@@ -1,25 +1,25 @@
 var mysql = require('mysql');
 
-  var db = mysql.createConnection({
-    host: "localhost",
-   user: "root",
-   password: "1997",
-   database : "ticdb"
-}); 
+//  var db = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "1997",
+//     database : "ticdb"
+// }); 
 
-var db = mysql.createConnection({
+/*var db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "victoria15",
     database: "usuarios"
-});
+});*/
 
-/* var db = mysql.createConnection({
+var db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "30234155",
     database: "ticdb"
-}); */
+});
 
 db.connect(function(err) {
     if (err) throw err;
